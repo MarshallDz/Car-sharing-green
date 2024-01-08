de85e271b19e7d671fcb15be5391620fa229372a
darked0 <eseverini10@gmail.com>
2024-01-08 10:44:43 +0100

    prova

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import sys
 from viste.welcome import WelcomeWindow
 import json
 
+
 def main():
     app = QApplication(sys.argv)
     welcome_window = WelcomeWindow()
     welcome_window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__":
     main()
--- viste/registrazione.py
+++ viste/registrazione.py
@@ -2,84 +2,89 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from Attivita.cliente import Cliente
+
+
 class VistaRegistrazione(QMainWindow):
-   def __init__(self):
-      super().__init__()
-      #dizionario in cui salvi i campi del form
-      self.campi = {}
+    def __init__(self):
+        super().__init__()
+        # dizionario in cui salvi i campi del form
+        self.campi = {}
 
-      self.setWindowTitle("Pagina di registrazione")
-      self.setGeometry(0, 0, QApplication.desktop().width(), QApplication.desktop().height())
-      self.setStyleSheet("background-color: #121212;")
+        self.setWindowTitle("Pagina di registrazione")
+        self.setGeometry(0, 0, QApplication.desktop().width(), QApplication.desktop().height())
+        self.setStyleSheet("background-color: #121212;")
 
-      self.central_widget = QWidget()
-      self.setCentralWidget(self.central_widget)
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
 
-      central_layout = QVBoxLayout()
+        central_layout = QVBoxLayout()
 
-      title_layout = QVBoxLayout()
-      title_layout.setAlignment(Qt.AlignTop | Qt.AlignCenter)
+        title_layout = QVBoxLayout()
+        title_layout.setAlignment(Qt.AlignTop | Qt.AlignCenter)
 
-      self.central_widget.setLayout(central_layout)
+        self.central_widget.setLayout(central_layout)
 
-      self.title_label = QLabel("Crea il tuo account")
-      self.title_label.setStyleSheet("color: white;")
-      self.title_font = QFont("Arial", 42, QFont.Bold)
-      self.title_label.setFont(self.title_font)
-      self.title_label.adjustSize()
+        self.title_label = QLabel("Crea il tuo account")
+        self.title_label.setStyleSheet("color: white;")
+        self.title_font = QFont("Arial", 42, QFont.Bold)
+        self.title_label.setFont(self.title_font)
+        self.title_label.adjustSize()
 
-      title_layout.addWidget(self.title_label)
-      central_layout.addLayout(title_layout)
+        title_layout.addWidget(self.title_label)
+        central_layout.addLayout(title_layout)
 
-      #layout di tutto il form
-      self.form_layout = QVBoxLayout()
-      self.form_layout.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
+        # layout di tutto il form
+        self.form_layout = QVBoxLayout()
+        self.form_layout.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
 
-      self.crea_campo("codice fiscale")
-      self.crea_campo("nome")
-      self.crea_campo("cognome")
-      self.crea_campo("data di nascita")
-      self.crea_campo("e-mail")
-      self.crea_campo("password")
-      self.crea_campo("cellulare")
+        self.crea_campo("codice fiscale")
+        self.crea_campo("nome")
+        self.crea_campo("cognome")
+        self.crea_campo("data di nascita")
+        self.crea_campo("e-mail")
+        self.crea_campo("password")
+        self.crea_campo("cellulare")
 
-      #creo i bottoni invia e indietro
-      invia_button = QPushButton("Invia")
-      invia_button.setStyleSheet("max-width: 200px; background-color: #D9D9D9; border-radius: 15px; color: black; padding: 10px;"
-                                 "margin-left: 35px; margin-top: 100px;")
-      back_button = QPushButton("Indietro")
-      back_button.setStyleSheet("max-width: 150px; background-color: #F85959; border-radius: 15px; color: black; padding: 10px;"
-                                 "margin-left: 60px;")
-      invia_button.clicked.connect(self.invio_dati)
-      back_button.clicked.connect(self.close)
-      self.form_layout.addWidget(invia_button)
-      self.form_layout.addWidget(back_button)
+        # creo i bottoni invia e indietro
+        invia_button = QPushButton("Invia")
+        invia_button.setStyleSheet(
+            "max-width: 200px; background-color: #D9D9D9; border-radius: 15px; color: black; padding: 10px;"
+            "margin-left: 35px; margin-top: 100px;")
+        back_button = QPushButton("Indietro")
+        back_button.setStyleSheet(
+            "max-width: 150px; background-color: #F85959; border-radius: 15px; color: black; padding: 10px;"
+            "margin-left: 60px;")
+        invia_button.clicked.connect(self.invio_dati)
+        back_button.clicked.connect(self.close)
+        self.form_layout.addWidget(invia_button)
+        self.form_layout.addWidget(back_button)
 
-      central_layout.addLayout(self.form_layout)
-   def crea_campo(self, nome):
-      if(nome == "data di nascita"):
-         campo = QDateEdit()
-         campo.setCalendarPopup(True)
-         campo.setDate(QDate.currentDate())
-      else:
-         campo = QLineEdit()
-         campo.setPlaceholderText(nome)
-         campo.setStyleSheet("max-width: 300px; min-height: 40px; background-color: #403F3F;")
-      self.campi[nome] = campo
-      self.form_layout.addWidget(campo)
+        central_layout.addLayout(self.form_layout)
 
-   def invio_dati(self):
-      data_to_save = {}
+    def crea_campo(self, nome):
+        if (nome == "data di nascita"):
+            campo = QDateEdit()
+            campo.setCalendarPopup(True)
+            campo.setDate(QDate.currentDate())
+        else:
+            campo = QLineEdit()
+            campo.setPlaceholderText(nome)
+            campo.setStyleSheet("max-width: 300px; min-height: 40px; background-color: #403F3F;")
+        self.campi[nome] = campo
+        self.form_layout.addWidget(campo)
 
-      # Extracting data from fields and formatting for JSON
-      for campo_nome, campo_widget in self.campi.items():
-         if isinstance(campo_widget, QLineEdit):
-            data_to_save[campo_nome] = campo_widget.text()
-         elif isinstance(campo_widget, QDateEdit):
-            data_to_save[campo_nome] = campo_widget.date().toString(Qt.ISODate)
-      cliente = Cliente()
-      print(data_to_save)
-      cliente.aggiungiCliente(data_to_save["codice fiscale"], data_to_save["nome"], data_to_save["cognome"],
-                              data_to_save["data di nascita"], data_to_save["e-mail"], data_to_save["password"], data_to_save["cellulare"])
-      self.close()
+    def invio_dati(self):
+        data_to_save = {}
 
+        # Extracting data from fields and formatting for JSON
+        for campo_nome, campo_widget in self.campi.items():
+            if isinstance(campo_widget, QLineEdit):
+                data_to_save[campo_nome] = campo_widget.text()
+            elif isinstance(campo_widget, QDateEdit):
+                data_to_save[campo_nome] = campo_widget.date().toString(Qt.ISODate)
+        cliente = Cliente()
+        print(data_to_save)
+        cliente.aggiungiCliente(data_to_save["codice fiscale"], data_to_save["nome"], data_to_save["cognome"],
+                                data_to_save["data di nascita"], data_to_save["e-mail"], data_to_save["password"],
+                                data_to_save["cellulare"])
+        self.close()
