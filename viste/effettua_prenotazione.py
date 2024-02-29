6724407f49320a3cdb42a5b1edd394bcc8f78f5a
michele <m.michelemarzioni@gmail.com>
2024-02-29 19:19:03 +0100

    roba nuova

--- viste/effettua_prenotazione.py
+++ viste/effettua_prenotazione.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import Qt, QDate
 
 class VistaEffettuaPrenotazione(QMainWindow):
     def __init__(self):
+
         super().__init__()
 
         self.setWindowTitle("Schermata di prenotazione")
